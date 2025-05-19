bab02564c0456e346b9eb9870207c63e56ac156f
IbrahimAbdAlrasol-SS <2255zeus.can@gmail.com>
2025-05-19 03:00:29 +0300

    خن

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
   port: 587,
-  secure: false, 
+  secure: false,
   auth: {
     user: process.env.EMAIL_ADDRESS,
     pass: process.env.GMAIL_PASSKEY, 
