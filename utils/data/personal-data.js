6434068af3db2130f670ad257dbdba2b3c7711a4
IbrahimAbdAlrasol-SS <2255zeus.can@gmail.com>
2025-05-27 04:00:11 +0300

    png

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ import { BsTelegram } from "react-icons/bs";
 
 export const personalData = {
   name: "Ibrahim Abdulrasool",
-  profile: '/profile.HEIC',
+  profile: '/profile.png',
   designation: "Desktop, Mobile & Bot Developer",
   description: "I’m Ibrahim Abdulrasool, a passionate and versatile software developer with a strong focus on mobile app development, desktop platforms, and Telegram bot automation. With solid experience in technologies like Flutter, Dart, Python, .NET, C++, C#, and Java, I build efficient and user-friendly applications that bridge functionality with performance.I specialize in building smart solutions powered by Firebase, MySQL, and Git version control. Currently, my main interest lies in crafting intelligent bots, automating workflows, and developing robust cross-platform mobile applications.",
   email: 's1123040@uomustansiriyah.edu.iq',
